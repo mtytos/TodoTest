412437e760f85d77c8cfaa09d650f2c7a00eecf0
VTB50651481@region.vtb.ru <mekke@vtb.ru>
2022-10-01 11:16:27 +0300

    lint add

---
+++ src/pages/Todo/index.tsx
@@ -0,0 +1,6 @@
+import { observer } from 'mobx-react';
+import { FC } from 'react';
+
+export const Todo: FC = () => {
+  return <></>;
+};
---
+++ src/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const AppStyled = styled.div`
+  margin: 0;
+  padding: 0;
+  text-align: center;
+`;
